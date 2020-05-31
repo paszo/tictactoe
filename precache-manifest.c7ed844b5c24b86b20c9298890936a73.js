3289b4ddd875c3c31b74504821acfa511478e328
paszo <paszo2008@gmail.com>
2020-05-31 14:18:43 +0200

    Updates

---
+++ precache-manifest.c7ed844b5c24b86b20c9298890936a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9163251c12bcff24670dbae78ac3da",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "e1c630347daa16fb00f3",
+    "url": "/tictactoe/static/css/main.769db0b0.chunk.css"
+  },
+  {
+    "revision": "788bf36ecea4c4a333c0",
+    "url": "/tictactoe/static/js/2.dc270b52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.dc270b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c630347daa16fb00f3",
+    "url": "/tictactoe/static/js/main.9533d31f.chunk.js"
+  },
+  {
+    "revision": "1d8dada0581fd2a39a6b",
+    "url": "/tictactoe/static/js/runtime-main.d9939438.js"
+  }
+]);
